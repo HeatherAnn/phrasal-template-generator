7dc91aa6252e70b62e9acc9cd5d18580aa0c8c16
HeatherAnn <heather.bisaillion@gmail.com>
2016-03-03 19:12:03 -0500

    Added index and a few prompts.

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var makeStory = alert('Let us make a story together!');
+
+var colour = prompt('A colour (orange, indigo, etc.)');
+
+var creature = prompt('A creature (dragon, moth, etc.)')
+
+Var adjective = prompt('An adjective (beautiful, super, etc.)');
