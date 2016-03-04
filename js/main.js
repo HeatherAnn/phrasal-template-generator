ef6ce32b25874aaeb573a129352171144d574814
HeatherAnn <heather.bisaillion@gmail.com>
2016-03-03 19:15:41 -0500

    Wrote all the prompts.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ var colour = prompt('A colour (orange, indigo, etc.)');
 
 var creature = prompt('A creature (dragon, moth, etc.)')
 
-Var adjective = prompt('An adjective (beautiful, super, etc.)');
+var adjective = prompt('An adjective (beautiful, super, etc.)');
+
+var verb = prompt('A past tense verb (ran, burped, etc.)');
+
+var story = prompt('Which story would you like? a) Snacking b) Escaping c) Winning \n Enter the letter below:');
